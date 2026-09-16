95a5633b57e4387f27ebf978dc6d38f1acbc8810
Matheus dos Santos <149119348+MS12112@users.noreply.github.com>
2026-09-16 10:05:43 -0300

    Create fundamentos.sql
    Primeiros exercícios

---
+++ fundamentos.sql
@@ -0,0 +1,16 @@
+-- Listar países em ordem alfabética
+SELECT country
+FROM country
+ORDER BY country;
+
+-- Top 10 PIB per capita
+SELECT country, gdp_pc
+FROM gdp_pc
+ORDER BY gdp_pc DESC
+LIMIT 10
+
+--
+
+--
+
+--
